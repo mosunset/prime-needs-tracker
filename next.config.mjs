ecf3e1a57d63e57204e0942ec02d3e8b5f771136
drivepc <driveasano@gmail.com>
2024-03-01 07:56:38 +0900

    icon

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,13 @@ const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
     images: {
-        // unoptimized:true, // すべての画像を最適化しない
-        remotePatterns: [ // 外部の画像を最適化する設定
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com'
-            },
-        ]
+        unoptimized: true, // すべての画像を最適化しない
+        // remotePatterns: [ // 外部の画像を最適化する設定
+        //     {
+        //         protocol: 'https',
+        //         hostname: 'lh3.googleusercontent.com'
+        //     },
+        // ]
     },
 };
 
